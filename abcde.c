0027e389238c41ffc136ec6103257478e41f5e3c
Xiang Gao <qasdfgtyuiop@gmail.com>
2017-09-13 00:07:14 -0400

    change to dpi

--- abcde.c
+++ abcde.c
@@ -1,6 +1,4 @@
 #include <net/netfilter/nf_tables.h>
-#include <net/tcp.h>
-#include <linux/ip.h>
 #include <linux/tcp.h>
 #include "abcde.h"
 
@@ -10,37 +8,17 @@ struct nft_abcde {
 	int len;
 };
 
-static inline bool modify_packet(struct nft_abcde *priv, struct sk_buff *skb) {
-	struct iphdr *iph = ip_hdr(skb);
+static inline bool match_packet(struct nft_abcde *priv, struct sk_buff *skb) {
 	struct tcphdr *tcph = tcp_hdr(skb);
 	char *user_data = (char *)((char *)tcph + (tcph->doff * 4));
 	char *tail = skb_tail_pointer(skb);
-	int tcplen = (skb->len - (iph->ihl << 2));
+	char *p;
 
-	if (!skb_make_writable(skb, skb->len))
-		return false;
-
-    printk(KERN_ERR "%s\n", user_data);
-	printk(KERN_ERR "%s\n", priv->text);
-
-	for (; user_data < tail - priv->len; user_data++) {
-		bool match = 1;
+	for (p = user_data; p < tail - priv->len; p++) {
 		int i;
 		for (i = 0; i < priv->len; i++)
-			if (user_data[i] != priv->text[i]) {
-				match = 0;
-				break;
-			}
-		if (match)
-			for (i = 0; i < priv->len; i++)
-				user_data[i] = 'x';
-	}
-
-	tcph->check = 0;
-	tcph->check = tcp_v4_check(tcplen, iph->saddr, iph->daddr, csum_partial((char *)tcph, tcplen, 0));
-	if (skb_checksum_complete(skb)) {
-		printk(KERN_ERR "abcde: wrong checksum");
-		return false;
+			if (p[i] != priv->text[i])
+				return false;
 	}
 	return true;
 }
@@ -52,7 +30,9 @@ static const struct nla_policy nft_abcde_policy[NFTA_ABCDE_MAX + 1] = {
 static void nft_abcde_eval(const struct nft_expr *expr, struct nft_regs *regs, const struct nft_pktinfo *pkt) {
 	struct nft_abcde *priv = nft_expr_priv(expr);
 	struct sk_buff *skb = pkt->skb;
-	if(!modify_packet(priv, skb))
+	if(match_packet(priv, skb))
+		regs->verdict.code = NFT_CONTINUE;
+	else
 		regs->verdict.code = NFT_BREAK;
 }
 
